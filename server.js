40720a385032187e2bfd4489565ebca0a1baf0b2
Andrey Balabanov <andrey.balabanov.email@mail.ru>
2025-08-07 20:33:34 +0700

    websocket logic is work and i dont have a shit why

--- server.js
+++ server.js
@@ -4,10 +4,69 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
+const ACTIONS = require("./src/socket/actions.js");
+
 var PORT = 3001;
 
+function getClientRooms () {
+    const {rooms} = io.sockets.adapter;
+    return Array.from(rooms.keys());
+}
+
+function shareRoomsInfo() {
+    io.emit(ACTIONS.SHARE_ROOMS, {
+        rooms: getClientRooms(), 
+    });
+}
+
 io.on("connection", socket => {
-    console.log("Socket connected");
+    shareRoomsInfo();
+    socket.on(ACTIONS.JOIN, config => {
+        const {room: roomID} = config;
+        const {rooms: joinedRooms} = socket;
+
+        if (Array.from(joinedRooms).includes(roomID)) {
+            return console.warn(`Already joined to ${roomID}`);
+        }
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+        clients.forEach(id => {
+            io.to(id).emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: false,
+            })
+
+            socket.emit(ACTIONS.ADD_PEER, {
+                peerID: id,
+                createOffer: true,
+            })
+        })
+
+        socket.join(roomID);
+        shareRoomsInfo();
+    })
+    
+    function leaveRoom() {
+        const {rooms} = socket;
+    
+        Array.from(rooms).forEach(room => {
+            const clients = Array.from(io.sockets.adapter.rooms.get(room) || []);
+            clients.forEach(clientID => {
+                io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+                    peerID: socket.id,
+                });
+    
+                socket.emit(ACTIONS.REMOVE_PEER, {
+                    peerID: clientID,
+                })
+            })
+            socket.leave(room);
+        })
+        shareRoomsInfo();
+    }
+    
+    socket.on(ACTIONS.LEAVE, leaveRoom);
+    socket.on("disconnecting", leaveRoom);
 });
 
 server.listen(PORT, function () {
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import socket from "../socket";
+import {useNavigate} from "react-router-dom"
+import {v4} from "uuid";
+
+const ACTIONS = require("../socket/actions");
 
 const Main = () => {
-    socket.io.on("error", () => {
-      console.log("Error");
-    });
+    const [rooms, updateRooms] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+            updateRooms(rooms);
+        })
+    }, [])
 
-    return <>This is the main page</>
+    return <>
+        <button onClick={() => {
+            navigate(`/room/${v4()}`);
+        }}>Create room</button>
+        Rooms: 
+        {rooms.map(id => 
+            <div key={id}>
+            {id}
+            <button onClick={() => {
+                navigate(`/room/${id}`);
+            }}>
+            JOIN
+            </button>
+        </div>)
+        }
+    </>
 }
 
 export default Main;
